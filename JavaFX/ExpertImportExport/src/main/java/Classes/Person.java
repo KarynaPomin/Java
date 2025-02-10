8fd1b9a54d7358a7899871d18bb32f31766cf8cf
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2025-02-10 14:59:18 +0100

    Add files via upload

---
+++ JavaFX/ExpertImportExport/src/main/java/Classes/Person.java
@@ -0,0 +1,13 @@
+package Classes;
+
+public class Person {
+    public String name;
+    public String login;
+    public String password;
+
+    public Person(String name, String login, String password) {
+        this.name = name;
+        this.login = login;
+        this.password = password;
+    }
+}
